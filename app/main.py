cc04cac0619613e99eadefe385800153d61414d5
canyurt <107627024+canyurt@users.noreply.github.com>
2025-11-10 13:01:57 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,3 +3,6 @@ from . import app
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=False)
+
+import os
+print(os.path.exists("/secrets/firebase-service-account.json"))
