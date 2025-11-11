d7cb9cc993ecc9451804486bd51e49bbf678a52d
canyurt <107627024+canyurt@users.noreply.github.com>
2025-11-11 11:28:12 +0100

    Delete app/main.py

--- app/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from . import app
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=False)
-
-import os
-print(os.path.exists("/secrets/firebase-service-account.json"))
